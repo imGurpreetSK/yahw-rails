a9737c271cb2ad64c117f18b80acc126a7854621
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-06-24 19:43:12 +0530

    Complete homework.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'root#root'
+  get '/about', to: 'about#about'
 end
