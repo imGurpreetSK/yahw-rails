328334d3538724e8ed5efcad5b43ea8665993538
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-06-25 22:28:53 +0530

    Work with scaffold and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'root#root'
+
   get '/about', to: 'about#about'
+
+  resources :articles
 end
