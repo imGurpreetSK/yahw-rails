e9c5871370c94fe926f15edd7a393d06c4990538
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-01 12:43:06 +0530

    Working bootstrap with rails.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+require("jquery")
+require("bootstrap-sprockets")
+//= require_tree .
+
 import "bootstrap"
