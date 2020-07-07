422883fb348c30c122a71de6f551bacdcb3625f3
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-07 22:40:17 +0530

    Extract email regex into a constant.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+
   validates :username, presence: true, length: 2...30, uniqueness: true
-  validates :email, presence: true, length: 5...110, uniqueness: true, email: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length: 5...110, uniqueness: true, email: true, format: EMAIL_REGEX
 end
