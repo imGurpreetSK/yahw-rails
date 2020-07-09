c474a14d416ea62f0f944587db4e5cf8ac9442e9
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-09 17:57:47 +0530

    Associate articles with users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
 
-  validates :username, presence: true, length: 2...30, uniqueness: true
-  validates :email, presence: true, length: 5...110, uniqueness: true, email: true, format: EMAIL_REGEX
+  has_many :articles
+
+  validates :username, presence: true, length: 2..30, uniqueness: true
+  validates :email, presence: true, length: 5..110, uniqueness: true, format: { with: EMAIL_REGEX }
 end
