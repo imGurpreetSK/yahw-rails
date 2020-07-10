e0c76999763fa588e0806c6420a3545f190aec01
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-10 23:08:05 +0530

    downcase email before saving to db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
 
+  before_save { self.email = email.downcase }
+
   has_many :articles
 
   validates :username, presence: true, length: 2..30, uniqueness: true
