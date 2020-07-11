ca6d0ca084b7b5b220dfb43dcc9149389bff8d60
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-11 21:36:21 +0530

    Create signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'root#root'
 
   get '/about', to: 'about#about'
+  get '/signup', to: 'users#new'
 
   resources :articles
+  resource :users, except: %i[new]
 end
