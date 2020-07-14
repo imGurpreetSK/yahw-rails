9e8d24633e482e2f40b49eeda818a87638b00f86
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-14 18:26:42 +0530

    Create all users index.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: %i[edit update]
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
