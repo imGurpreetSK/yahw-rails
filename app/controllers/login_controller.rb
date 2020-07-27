5410eb30a83b5527629a42a61ad62cfc10b80e29
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-07-27 22:41:04 +0530

    Add ability for user to login.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,15 @@ class LoginController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:login][:email].downcase)
+    if user&.authenticate(params[:login][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user
+    else
+      flash.now[:alert] = 'Check login details and retry'
+      render 'new'
+    end
   end
 
   def destroy
